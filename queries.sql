660a4a39a75465b629a20fba680b4509b7ba32af
Mohammed Rehaan <73173381+mohammed-rehaan@users.noreply.github.com>
2021-10-06 17:49:19 +0530

    first

---
+++ queries.sql
@@ -0,0 +1,64 @@
+create table sem1(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem2(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem3(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem4(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem5(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem6(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem7(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+create table sem8(
+  fname VARCHAR(255) NOT NULL,
+  mname VARCHAR(255) ,
+  lname VARCHAR(255) NOT NULL,
+  usn VARCHAR(11) UNIQUE,
+  mobile BIGINT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
